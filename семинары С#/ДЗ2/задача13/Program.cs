3da096b58c9100f708f0ab69955f185e00858e4e
Genniiy <115341347+Genniiy@users.noreply.github.com>
2023-02-04 00:51:31 +0300

    Add files via upload

---
+++ семинары С#/ДЗ2/задача13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Введите число  ");
+
+string s = Console.ReadLine();
+int a = int.Parse(s);
+if (a < 99) {
+    Console.WriteLine("третьей цифры нет");
+}
+Console.WriteLine(Convert.ToString(s[2]));
+ 
